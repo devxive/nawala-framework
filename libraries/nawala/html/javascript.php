92da3dfca0c592e74bd299bf9d3aaf192238467d
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-07-02 22:35:45 +0200

    added more methods
    prevent site leaving
    prevent keypress form submissions
    checkout messages
    gritter support
    TODO bindWindowAlertMessages

--- libraries/nawala/html/javascript.php
+++ libraries/nawala/html/javascript.php
@@ -165,7 +165,7 @@ abstract class NHtmlJavaScript
 		NHtml::loadJsFramework();
 
 		// Include dependencies
-		self::dependencies('devxive.sitereadyoverlay');
+		self::dependencies('nawala.sitereadyoverlay');
 
 		// Attach the function to the document
 		JFactory::getDocument()->addScriptDeclaration(
@@ -409,7 +409,7 @@ abstract class NHtmlJavaScript
 		// Attach the function to the document
 		JFactory::getDocument()->addScriptDeclaration(
 			"jQuery('" . $selector . "').on('click', function () {
-				$('" . $selector . "').addClass("btn-warning");
+				$('" . $selector . "').addClass('btn-warning');
 				var btn = $(this);
 				btn.button('loading')
 				setTimeout(function () {
@@ -461,6 +461,244 @@ abstract class NHtmlJavaScript
 		return;
 	}
 
+	/**
+	 * Add javascript support to prevent to leave the site, if anything in the form has changed and is not saved at present!
+	 *
+	 * @param	string		$selector	Common id for the form or the formfields. Standard: "form" as the form element
+	 *
+	 * @return  void
+	 *
+	 * @since   10.9
+	 */
+	public function setPreventFormLeaveIfChanged($selector = 'form')
+	{
+		$sig = md5(serialize(array($selector)));
+
+		// Only load once
+		if (isset(self::$loaded[__METHOD__][$sig]))
+		{
+			return;
+		}
+
+		// Include JS frameworks
+		NHtml::loadJsFramework();
+
+		// Attach the function to the document
+		JFactory::getDocument()->addScriptDeclaration(
+			"var catcher = function() {
+				var changed = false;
+
+				$('" . $selector . "').each(function() {
+					if ($(this).data('initialForm') != $(this).serialize()) {
+						changed = true;
+						$(this).addClass('changed');
+					} else {
+						$(this).removeClass('changed');
+					}
+				});
+
+				if (changed) {
+					return '" . JText::_('COM_XIVEIRM_PREVENT_LEAVE_SITE') . "';
+				}
+			};
+
+			jQuery(function() {
+				$('" . $selector . "').each(function() {
+					$(this).data('initialForm', $(this).serialize());
+				}).submit(function(e) {
+					var formEl = this;
+					var changed = false;
+
+					$('" . $selector . "').each(function() {
+						if (this != formEl && $(this).data('initialForm') != $(this).serialize()) {
+							changed = true;
+							$(this).addClass('changed');
+						} else {
+							$(this).removeClass('changed');
+						}
+					});
+
+					// If we have 2 or more forms on this page - Be careful with hidden forms!!
+					if (changed && !confirm('" . JText::_('COM_XIVEIRM_PREVENT_FORM_SUBMISSION') . "')) {
+						e.preventDefault();
+					} else {
+						$(window).unbind('beforeunload', catcher);
+					}
+				});
+
+				$(window).bind('beforeunload', catcher);
+			});\n"
+		);
+
+		self::$loaded[__METHOD__][$sig] = true;
+
+		return;
+	}
+
+	/**
+	 * Add javascript support for growl like notification messages via Gritter
+	 *
+	 * @param	string		$selector	
+	 *
+	 * @return  void
+	 *
+	 * @since   8.0
+	 */
+	public function loadGritter($selector = 'gritter')
+	{
+		$sig = md5(serialize(array($selector)));
+
+		// Only load once
+		if (isset(self::$loaded[__METHOD__][$sig]))
+		{
+			return;
+		}
+
+		// Include JS framework
+		NHtml::loadJsFramework();
+
+		// Include dependencies
+		self::dependencies('nawala.gritter');
+
+		// Attach the function to the document
+//		JFactory::getDocument()->addScriptDeclaration(
+//			"jQuery(document).ready(function() {
+//				$('" . $selector . "').autosize();
+//			});\n"
+//		);
+
+		self::$loaded[__METHOD__][$sig] = true;
+
+		return;
+	}
+
+	/**
+	 * Add javascript support for checked out by another user messages
+	 * Shows a predesigned messagebox with informations about the checkout process and the user who checked out the item
+	 *
+	 * @param   int     $checkoutUserId  The ID of the user who checked out the item as set in table, row checked_out.
+	 * @param   int     $checkoutTime    The ID of the user who checked out the item as set in table, row checked_out.
+	 * @param   string  $selector        The ID selector for the tooltip. Can also be .xTooltip, [data-rel=tooltip], #hasMyTip
+	 * @param   array   $params          An array of options for the tooltip.
+	 *                                   Options for the tooltip can be:
+	 *                                   - animation  boolean          A jQuery function. Standard: "slideDown"
+	 *                       - checkoutByOtherTitle   string           Title if the item is checked out by another user for JText::_('')
+	 *                     - checkoutByOtherMessage   string           Message value for JText::sprintf('',)
+	 *                                                                      1. Value is the name of the user.
+	 *                                                                      2. Time as set in checked_out_time row of appropriate table.
+	 *                                                                      3. [Optional] Time in minutes if the item might be checked in.
+	 *                       - checkoutByUserTitle    string           Title if the item is checked out by user itself for JText::_('')
+	 *                     - checkoutByUserMessage    string           Message value for JText::sprintf('',)
+	 *                                                                      1. Time as set in checked_out_time row of appropriate table.
+	 *                            - checkinMessage    string           Message to display if a time is set when the item is checked in again
+	 *                               - checkinTime    string           Provide a time in minutes when the item is chekced in again (may with an external script or something else)
+	 *                                   - duration   string           Duration of the animation - slow | fast
+	 *                                   - delay      number           Delay after the checkout is message showing (ms)
+	 *
+	 * @return  false                                                  If nothing is checked out
+	 * ...@return  mixed, array                                        Load scriptDeclaration for showing the message container
+	 *                                  - $results['by']               Checked out by - other | user | false
+	 *                                  - $results['message']          The html message to set wherever you want in the doc
+	 *
+	 * @since   13.4
+	 */
+	public function getCheckoutMessage($checkoutUserId, $checkoutTime, $selector = '#checkout-message', $params = array())
+	{
+		// Check if it is checked out
+		if($checkoutUserId == 0)
+		{
+			return false;
+		}
+
+		$sig = md5(serialize(array($selector)));
+
+		// Only load once
+		if (isset(self::$loaded[__METHOD__][$sig]))
+		{
+			return;
+		}
+
+		// Include JS framework
+		NHtml::loadJsFramework();
+
+		// Get id from the user object
+		$currentUserId = JFactory::getUser()->id;
+		$checkedOutTime = date(JText::_('DATE_FORMAT_LC2'), strtotime($checkoutTime));
+		$checkinIn = '10';
+
+		// Setup options object
+		$opt['checkoutByOtherTitle']	= (isset($params['checkoutByOtherTitle']) && ($params['checkoutByOtherTitle'])) ? (string) $params['checkoutByOtherTitle'] : 'This item has been checked out by another user!';
+		$opt['checkoutByOtherMessage']	= (isset($params['checkoutByOtherMessage']) && ($params['checkoutByOtherMessage'])) ? (string) $params['checkoutByOtherMessage'] : 'This Item has been checked out by %s at %s. You can\'t edit this until this is checked in.';
+		$opt['checkoutByUserTitle']		= (isset($params['checkoutByUserTitle']) && ($params['checkoutByUserTitle'])) ? (string) $params['checkoutByUserTitle'] : 'This Item has been checked out by yourself!';
+		$opt['checkoutByUserMessage']	= (isset($params['checkoutByUserMessage']) && ($params['checkoutByUserMessage'])) ? (string) $params['checkoutByUserMessage'] : 'You have checked out this item at %s';
+		$opt['checkinMessage']		= (isset($params['checkinMessage']) && ($params['checkinMessage'])) ? (string) $params['checkinMessage'] : 'A checkin is attempted within the next ~%s minutes';
+		$opt['checkinTime']	= (isset($params['checkinTime']) && ($params['checkinTime'])) ? (string) $params['checkinTime'] : null;
+		$opt['userlink']	= (isset($params['userlink']) && ($params['userlink'])) ? (string) $params['userlink'] : null;
+		$opt['animation']	= (isset($params['animation']) && ($params['animation'])) ? (int) $params['animation'] : 'slideDown';
+		$opt['duration']	= (isset($params['duration']) && ($params['duration'])) ? (int) $params['duration'] : 'slow';
+		$opt['delay']		= (isset($params['delay']) && ($params['delay'])) ? (int) $params['delay'] : 2000;
+
+//		$options = NHtml::getJSObject($opt);
+
+		// Build the user who has checked out the item
+		if($opt['userlink']) {
+			$checkedOutUser = '<a href="" target="_blank">' . JFactory::getUser($checkoutUserId)->name . '</a>';
+		} else {
+			$checkedOutUser = JFactory::getUser($checkoutUserId)->name;
+		}
+
+		// Based on the above check, we're in active check out. Now identify who has checked out and return html
+		$html_out = '';
+		$results = array();
+
+		$cssSelector = preg_replace('#[^A-Z0-9-_]#i', '', $selector);
+
+		if($checkoutUserId != $currentUserId)
+		{
+			$results['by'] = 'other';
+
+			$html_out .= '<div id="' . $cssSelector . '" class="alert alert-error" style="display: none;">';
+			$html_out .= '<button type="button" class="close" data-dismiss="alert">';
+			$html_out .= '<i class="icon-remove"></i>';
+			$html_out .= '</button>';
+			$html_out .= '<h1><i class="icon-signout"></i> ' . JText::_($opt['checkoutByOtherTitle']) . '</h1>';
+			$html_out .= '<p>' . JText::sprintf($opt['checkoutByOtherMessage'], $checkedOutUser, $checkedOutTime) . '</p>';
+			if($opt['checkinMessage']) {
+				$html_out .= '<p>' . JText::sprintf($opt['checkinMessage'], $opt['checkinTime']) . '</p>';
+			}
+			$html_out .= '</div>';
+		}
+		else
+		{
+			$results['by'] = 'user';
+
+			$html_out .= '<div id="' . $cssSelector . '" class="alert alert-notice" style="display: none;">';
+			$html_out .= '<button type="button" class="close" data-dismiss="alert">';
+			$html_out .= '<i class="icon-remove"></i>';
+			$html_out .= '</button>';
+			$html_out .= '<h1><i class="icon-signout"></i> ' . JText::_($opt['checkoutByUserTitle']) . '</h1>';
+			$html_out .= '<p>' . JText::sprintf($opt['checkoutByUserMessage'], $checkedOutTime) . '</p>';
+			$html_out .= '</div>';
+		}
+
+		$results['message'] = $html_out;
+
+		// Attach the function to the document
+		JFactory::getDocument()->addScriptDeclaration(
+			"jQuery(window).load(function() {
+				if ($('" . $selector . "').is(':hidden')) {
+					setTimeout(function () {
+						jQuery('" . $selector . "')." . $opt['animation'] . "('" . $opt['duration'] . "');
+					}, " . $opt['delay'] . ");
+				}
+			});\n"
+		);
+
+		self::$loaded[__METHOD__][$sig] = true;
+
+		return $results;
+	}
+
 	 /*
 	 * Load dependencies for this class
 	 *
@@ -490,9 +728,9 @@ abstract class NHtmlJavaScript
 			JHtml::_('script', 'nawala/jquery.ui.effects.js', false, true, false, false, $debug);
 		}
 
-		if($type === 'devxive.sitereadyoverlay')
+		if($type === 'nawala.sitereadyoverlay')
 		{
-			JHtml::_('stylesheet', 'nawala/devxive.sitereadyoverlay.css', false, true);
+			JHtml::_('stylesheet', 'nawala/nawala.sitereadyoverlay.css', false, true);
 		}
 
 		if($type === 'jquery.inputlimiter')
@@ -505,8 +743,56 @@ abstract class NHtmlJavaScript
 			JHtml::_('script', 'nawala/jquery.autosize.min.js', false, true, false, false, $debug);
 		}
 
+		if($type === 'nawala.gritter')
+		{
+			JHtml::_('stylesheet', 'nawala/nawala.gritter.bootstrap.css', false, true);
+			JHtml::_('script', 'nawala/nawala.gritter.bootstrap.js', false, true, false, false, $debug);
+		}
+
+		if($type === 'jquery.gritter')
+		{
+			JHtml::_('stylesheet', 'nawala/jquery.gritter.css', false, true);
+			JHtml::_('script', 'nawala/jquery.gritter.min.js', false, true, false, false, $debug);
+		}
+
 		self::$loaded[__METHOD__][$sig] = true;
 
 		return;
 	}
+
+// TODO:	function bindWindowAlertMessages()
+//	{
+//		// if (_alert) return;
+//		// var _alert = window.alert;
+//		jQuery(window).load(function() {
+//			alert('Hilfe');
+//		});
+//		
+//		jQuery(function(){
+//			window.alert = function(message) {
+//				$.gritter.add({
+//					title: 'Alert',
+//					text: message,
+//					icon: 'icon-warning-sign',
+//					class_name: 'alert-error'
+//				});
+//			};
+//			window.confirm = function(message) {
+//				$.gritter.add({
+//					title: 'Alert',
+//					text: message,
+//					icon: 'icon-warning-sign',
+//					class_name: 'alert-error'
+//				});
+//			};
+//			window.prompt = function(message) {
+//				$.gritter.add({
+//					title: 'Alert',
+//					text: message,
+//					icon: 'icon-warning-sign',
+//					class_name: 'alert-error'
+//				});
+//			};
+//		});
+//	}
 }
