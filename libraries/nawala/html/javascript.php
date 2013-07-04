e18e7e5613e7e685f4370e56d60e82df48d98915
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-07-04 06:25:11 +0200

    added setChosen Class // chosen js support

--- libraries/nawala/html/javascript.php
+++ libraries/nawala/html/javascript.php
@@ -573,6 +573,89 @@ abstract class NHtmlJavaScript
 	}
 
 	/**
+	 * Add javascript support for chosen select lists
+	 *
+	 * @param   string  $selector  Selector for the tooltip
+	 * @param   string  $trigger   ID of the form field for updating chosen dynamically (without the id selector: #). If you dont want it, set trigger to false.
+	 *                             Desc: If you need to update the options in your select field and want Chosen to pick up the changes, you'll need
+	 *                                   to trigger the "liszt:updated" event on the field. Chosen will re-build itself based on the updated content.
+	 * @param   array   $params    An array of options for the tooltip.
+	 *
+	 *                  Options for the tooltip can be:
+	 *                      disable_search_threshold  int              Option to specify to hide the search input on single selects if there are fewer than (n) options.
+	 *                      disable_search            boolean          Option to disable the search. true | false (standard)
+	 *                      no_results_text           string           Setting the "No results" search text - Example: Oops, nothing found! or JText::_('PLACEHOLDER')
+	 *                      max_selected_options      int|function     Limit how many options can user select
+	 *                      allow_single_deselect     boolean          When a single select box isn't a required field, you can set allow_single_deselect: true
+	 *                                                                 Chosen will add a UI element for option deselection. This will only work if the first option has blank text.
+	 *                      width                     string           Using a custom width with chosen. Example: '95%'
+	 *
+	 *                                          Note: On single selects, the first element is assumed to be selected by the browser.
+	 *                                                To take advantage of the default text support, you will need to include a blank option as the first element of your select list.
+	 *
+	 *                                                Example:
+	 *                                                        <?php NHtmlJavaScript::setChosen('.chzn-select', 'form_gender_field', array('allow_single_deselect' => true, 'width' => '95%')); ?>
+	 *                                                        <select id="form_gender_field" class="chzn-select" name="form_gender" data-placeholder="Choose a gender...">
+	 *                                                            <option value></option>
+	 *                                                            <option value="female">Female</option>
+	 *                                                            <option value="male">Male</option>
+	 *                                                        </select>
+	 *
+	 * @return  void
+	 *
+	 * @see http://harvesthq.github.io/chosen/ for more informations and options
+	 *
+	 * @since   13.7
+	 */
+	public function setChosen($selector = '.chosen-select', $trigger = false, $params = array())
+	{
+		$sig = md5(serialize(array($selector, $trigger)));
+
+		// Only load once
+		if (isset(self::$loaded[__METHOD__][$sig]))
+		{
+			return;
+		}
+
+		// Include JS framework
+		NHtml::loadJsFramework();
+
+		// Include dependencies
+		self::dependencies('jquery.chosen');
+
+		// Setup options object
+		$opt['disable_search_threshold'] = isset($params['disable_search_threshold']) ? $params['disable_search_threshold'] : null;
+		$opt['disable_search'] = isset($params['disable_search']) ? $params['disable_search'] : null;
+		$opt['no_results_text'] = isset($params['no_results_text']) ? $params['no_results_text'] : null;
+		$opt['max_selected_options'] = isset($params['max_selected_options']) ? $params['max_selected_options'] : null;
+		$opt['allow_single_deselect'] = isset($params['allow_single_deselect']) ? $params['allow_single_deselect'] : null;
+		$opt['width'] = isset($params['width']) ? $params['width'] : null;
+
+		$options = NHtml::getJSObject($opt);
+
+		// Build the scriptDeclaration
+		if(!$trigger) {
+			$srciptDec = 
+				"jQuery(document).ready(function() {
+					$('" . $selector . "').chosen(" . $options . ");
+				});\n";
+		} else {
+			$srciptDec = 
+				"jQuery(document).ready(function() {
+					$('" . $selector . "').chosen(" . $options . ");
+					$('#" . $trigger . "').trigger('liszt:updated');
+				});\n";
+		}
+
+		// Attach the function to the document
+		JFactory::getDocument()->addScriptDeclaration($srciptDec);
+
+		self::$loaded[__METHOD__][$sig] = true;
+
+		return;
+	}
+
+	/**
 	 * Add javascript support for checked out by another user messages
 	 * Shows a predesigned messagebox with informations about the checkout process and the user who checked out the item
 	 *
@@ -753,6 +836,13 @@ abstract class NHtmlJavaScript
 		{
 			JHtml::_('stylesheet', 'nawala/jquery.gritter.css', false, true);
 			JHtml::_('script', 'nawala/jquery.gritter.min.js', false, true, false, false, $debug);
+		}
+
+		if($type === 'jquery.chosen')
+		{
+			JHtml::_('stylesheet', 'nawala/jquery.chosen.min.css', false, true);
+			JHtml::_('stylesheet', 'nawala/nawala.chosen.css', false, true);
+			JHtml::_('script', 'nawala/jquery.chosen.min.js', false, true, false, false, $debug);
 		}
 
 		self::$loaded[__METHOD__][$sig] = true;
