9805376a765f67de7180401b9a2b3580231a4f5a
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-07-31 01:06:48 +0200

    added setup instructions

---
+++ source/setup.php
@@ -0,0 +1,26 @@
+﻿<?php
+/**
+ * @project		XAP Project - Xive-Application-Platform
+ * @subProject	Nawala Framework - A PHP and Javascript framework
+ *
+ * @package		NFW.Installer
+ * @subPackage	Framework
+ * @version		6.0
+ *
+ * @author		devXive - research and development <support@devxive.com> (http://www.devxive.com)
+ * @copyright		Copyright (C) 1997 - 2013 devXive - research and development. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @assetsLicense	devXive Proprietary Use License (http://www.devxive.com/license)
+ *
+ * @since		5.0
+ */
+
+$message = array();
+
+$message[] = '<i class="icon-cancel"></i> Removed installation files';
+
+if (!empty($message)) {
+	return $message;
+} else {
+	return true;
+}
