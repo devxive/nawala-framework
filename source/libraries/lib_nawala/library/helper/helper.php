3855bd73b0803b741cc6f495d1e015bc345b4a5c
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-08-09 04:06:27 +0200

    added helper class

---
+++ source/libraries/lib_nawala/library/helper/helper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @version     5.0.0
+ * @package     NAWALA FRAMEWORK
+ * @subPackage  NHelper
+ * @copyright   Copyright (C) 1997 - 2013 by devXive - research and development. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      devXive <support@devxive.com> - http://devxive.com
+ */
+
+defined('_JEXEC') or die;
+
+abstract class NHelper
+{
+}
